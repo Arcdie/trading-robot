1ffd4c20d6633183793b92c8b8b3de46518798ee
Valentyn <xdshkaa2@gmail.com>
2021-12-27 08:44:42 +0200

    master: added message

--- services/trading-helper-screener/get-price-rebounds.js
+++ services/trading-helper-screener/get-price-rebounds.js
@@ -11,6 +11,7 @@ const {
 } = require('../../controllers/strategies/priceRebounds/utils/init-trade-process');
 
 const {
+  app: { isTestMode },
   tradingHelperScreenerConf,
 } = require('../../config');
 
@@ -54,6 +55,10 @@ module.exports = async () => {
 
       client.on('message', async bufferData => {
         const parsedData = JSON.parse(bufferData.toString());
+
+        const m = `${isTestMode ? 'Test' : 'Production'}: new strategyTargetId: ${parsedData.strategyTargetId}`;
+        // log.warn(m);
+        console.log(m);
 
         const resultInit = await initTradeProcess(parsedData.data);
 
